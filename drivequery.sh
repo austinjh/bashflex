906bc3b83b7b28e82e181239da6ca385c7d85825
Austin Holdsworth <austin.holdsworth@gmail.com>
2022-07-14 23:18:43 +0100

    Improved error checking

--- drivequery.sh
+++ drivequery.sh
@@ -5,20 +5,25 @@ set -e
 # az account set --subscription uob-res-dev 
 
 declare -a diskseq=(c d e f g h i j k l m o p q r s t u v w x y z)
-declare -a disksizes=(100 100 32)
-declare -a disksizesnew=(100 100 100 32)
+declare -a disksizesnew=(100 100)
 declare -a pvgroup=()
 DATADISK_MAX=16
 DATADISK_MAXSIZE=32768
 disknameprefix="nimbus-dev-camptest-1-dev-sd"
-
+RESOURCE_NAME=vm1
 RESOURCE_GROUP=rg-res-dev-research2-camptest-1
+NFSSHAREVM_RG=$RESOURCE_GROUP
+NFS_DATADISK_SKU=Standard
 
-az disk list --resource-group $RESOURCE_GROUP --query "[*].{Name:name,Gb:diskSizeGb,Tier:sku.tier}" --output table
+currentdiskqty=$(az disk list --resource-group $RESOURCE_GROUP --query "[?contains(name,'${disknameprefix}')].{Name:name,Gb:diskSizeGb,Tier:sku.tier}" --output table | grep --count sd)
 
 if (( ${#disksizesnew[@]} > $DATADISK_MAX )); then
     echo "Error: Maximum disks reached"
     exit 1
+fi
+if (( ${#disksizesnew[@]} < $currentdiskqty )); then
+    echo "Error: Unable to provision fewer disks than currently attached"
+    exit 3
 fi
 
 re='^[0-9]+$'
@@ -37,13 +42,10 @@ for i in ${!disksizesnew[@]} ; do
     diskname=${diskseq[$i]}
     disksizecurrent=$(az disk list --resource-group $RESOURCE_GROUP --query "[?contains(name,'${disknameprefix}${diskname}')].diskSizeGb" --output tsv)
     disksizeproposed=${disksizesnew[$i]}
-    #echo "Test $disksizecurrent Current"
-    #echo "Test $disksizeproposed Proposed"
-    #echo ${disknameprefix}${diskname} is sized at $disksizecurrent
     if [ -z $disksizecurrent ]
     then
         echo "Disk will be added, size ${disksizeproposed}G"
-        echo az vm disk attach --name ${disknameprefix}${diskname} --resource-group $RESOURCE_GROUP --size-gb $disksizeproposed
+        echo az vm disk attach --vm-name ${RESOURCE_NAME} --resource-group $NFSSHAREVM_RG --name ${disknameprefix}${diskname} --size-gb ${disksizeproposed} --sku $NFS_DATADISK_SKU --new
     else
         if (( $disksizeproposed > $disksizecurrent ))
         then
@@ -52,7 +54,7 @@ for i in ${!disksizesnew[@]} ; do
         else
             echo "NO CHANGE - Disk ${disknameprefix}${diskname} will remain at ${disksizecurrent}G"
             if (( $disksizeproposed < $disksizecurrent )); then
-                echo "Disks are not permmited to be smaller than they are currently"
+                echo "Updated disk sizes are not permmited to be smaller than those currently attached"
                 exit 2
             fi
         fi
